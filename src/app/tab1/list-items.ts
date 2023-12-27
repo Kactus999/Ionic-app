79bbbafe83158b91c76e1c9a5227d4b5b56db138
Kactus999 <fuzzy167it00@gmail.com>
2023-12-27 09:58:15 +0700

    use ngFor to display items

---
+++ src/app/tab1/list-items.ts
@@ -0,0 +1,6 @@
+export interface ListItems {
+  id: number;
+  color: string;
+  iconName: string;
+  title: string;
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,10 +1,92 @@
 import { Component } from '@angular/core';
-
+import { ListItems } from './list-items';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  constructor() {}
+  itemList: ListItems[] = [
+    {
+      id: 0,
+      color: 'rgb(130, 37, 130)',
+      iconName: 'notifications-outline',
+      title: 'Quản lý thông báo mới',
+    },
+    {
+      id: 1,
+      color: 'rgb(80, 186, 251)',
+      iconName: 'gift-outline',
+      title: 'Rewards',
+    },
+  ];
+
+  upComingItemList: ListItems[] = [
+    {
+      id: 2,
+      color: 'rgb(250,66,66)',
+      iconName: 'Person-circle-outline',
+      title: 'Thay đổi thông tin',
+    },
+    {
+      id: 3,
+      color: 'blue',
+      iconName: 'folder-outline',
+      title: 'Tài liệu',
+    },
+    {
+      id: 4,
+      color: 'rgb(245,65,29)',
+      iconName: 'share-social-outline',
+      title: 'Chia sẻ',
+    },
+    {
+      id: 5,
+      color: 'rgb(77,251,46)',
+      iconName: 'checkbox-outline',
+      title: 'Ký duyệt hồ sơ',
+    },
+    {
+      id: 6,
+      color: 'rgb(249,49,197)',
+      iconName: 'briefcase-outline',
+      title: 'Báo cáo',
+    },
+    {
+      id: 7,
+      color: 'rgb(130,37,130)',
+      iconName: 'stats-chart-outline',
+      title: 'BBQ & eApp',
+    },
+    {
+      id: 8,
+      color: 'rgb(80,186,251)',
+      iconName: 'checkbox-outline',
+      title: 'Học trực tuyến',
+    },
+    {
+      id: 9,
+      color: 'rgb(245,65,29)',
+      iconName: 'briefcase-outline',
+      title: 'eFNA',
+    },
+    {
+      id: 10,
+      color: 'rgb(113,98,113)',
+      iconName: 'stats-chart-outline',
+      title: 'Quản lý khách hàng',
+    },
+  ];
+  chunkedItems: any[] = [];
+  constructor() {
+    this.chunkedItems = this.chunkArray(this.upComingItemList, 3);
+  }
+
+  chunkArray(arr: any[], size: number): any[] {
+    const chunkedArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+      chunkedArr.push(arr.slice(i, i + size));
+    }
+    return chunkedArr;
+  }
 }
